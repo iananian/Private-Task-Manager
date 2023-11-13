253a89f0171a9575068817d4533b15aff7f07a57
ian <a87g1281@gmail.com>
2023-11-13 13:41:56 +0900

    add main_GUI.py

---
+++ main GUI.py
@@ -0,0 +1,202 @@
+# GUI 모듈 임포트함.
+import tkinter as tk
+from tkinter import messagebox
+# pickle모듈을 이용해 데이터를 로컬로 저장. / web서비스시 서버에 저장하도록 수정 가능.
+import pickle
+
+# 파일에 저장되는 tasks 데이터를 담을 파일 이름.
+TASKS_FILE = "tasks_data.pkl"
+
+# task를 저장하기 위한 list를 제작.
+# task의 양식은 다음 과 같음. (task, deadline, status)
+tasks = []
+
+# 파일에서 tasks를 불러오기 위한 기능.
+def load_tasks():
+    # 함수 내에서 전역 변수를 수정하기 위한 작업.
+    global tasks
+    try:
+        with open(TASKS_FILE, 'rb') as file:
+            tasks = pickle.load(file)
+    except FileNotFoundError:
+        print("save된 file을 찾을 수 없음. 새로운 파일을 생성함.")
+
+# tasks를 파일에 저장하기 위한 기능.
+def save_tasks():
+    with open(TASKS_FILE, 'wb') as file:
+        pickle.dump(tasks, file)
+
+# 태스크를 추가하기 위한 기능.
+def add_task():
+    # 데드라인은 년, 월, 일을 각각 작성하기 위해 list로 작성.
+    deadline = [0, 0, 0]
+    
+    # 요소들을 input받음.
+    task = input("Task의 내용을 작성\n")
+    try:
+        deadline[0], deadline[1], deadline[2] = input("Task의 Deadline을 작성 / 양식 : '년 월 일'\n").split(" ")
+    except ValueError:
+        deadline[0], deadline[1], deadline[2] = input("Task의 Deadline을 다시 작성 / 양식 : '년 월 일'\n").split(" ")
+
+    # input받은 요소들을 tasks리스트에 저장
+    # status의 기본값은 false
+    if task:
+        tasks.append((task, deadline, False))
+    else:
+        print("경고, Task를 작성")
+
+
+# 모든 태스크를 보여주기 위한 기능.
+def sort_by_order():
+    # tasks를 리스트업
+    for i in tasks:
+        if i[2] == False:
+            print("Task:", i[0], "Deadline:", i[1], "Status:", i[2])
+
+
+# 데드라인순으로 태스크를 정렬하기 위한 기능.
+def sort_by_deadline():
+    # order값을 기준으로 tasks를 정렬
+    tasks_sort_deadline = sorted(tasks, key=lambda x: (x[1][0], x[1][1], x[1][2]))
+
+    # tasks를 리스트업
+    for i in tasks_sort_deadline:
+        if i[2] == False:
+            print("Task:", i[0], "Deadline:", i[1], "Status:", i[2])
+
+
+# 태스크를 complete로 표시하기 위한 기능.
+def complete_task():
+    # 함수 내에서 전역 변수를 수정하기 위한 작업.
+    global tasks
+
+    # tasks가 없다면 return하기 위한 기능.
+    if not tasks:
+        print("Task가 없음")
+        return
+
+    # 예외 경우가 발생하면 처리하기 위해 try를 이용.
+    try:
+        status = int(input("완료할 Task의 번호를 선택\n"))
+        if status >= 0 and status <= len(tasks):
+            if not tasks[status][2]:
+                del tasks[status]
+                print("Task가 완료로 표시됨")
+            else:
+                print("이미 완료된 Task")
+        else:
+            print("유효한 번호가 아님")
+    except ValueError:
+        print("유효한 숫자를 입력")
+
+# 이 아래는 chat-GPT draft임.
+def update_listbox():
+    listbox.delete(0, tk.END)
+    for i, task in enumerate(tasks):
+        listbox.insert(tk.END, f"Task {i}: {task[0]} | Deadline: {' '.join(map(str, task[1]))} | Status: {task[2]}")
+
+def add_task_gui():
+    task = entry_task.get()
+    deadline = entry_deadline.get().split()
+    try:
+        deadline = [int(d) for d in deadline]
+    except ValueError:
+        messagebox.showwarning("입력 오류", "유효한 년 월 일을 입력하세요.")
+        return
+
+    if task:
+        tasks.append((task, deadline, False))
+        update_listbox()
+    else:
+        messagebox.showwarning("입력 오류", "Task를 작성하세요.")
+
+def sort_by_order_gui():
+    listbox.delete(0, tk.END)
+    for i, task in enumerate(tasks):
+        listbox.insert(tk.END, f"Task {i}: {task[0]} | Deadline: {' '.join(map(str, task[1]))} | Status: {task[2]}")
+
+def sort_by_deadline_gui():
+    sorted_tasks = sorted([task for task in tasks if not task[2]], key=lambda x: x[1])
+
+    listbox.delete(0, tk.END)
+    for i, task in enumerate(sorted_tasks):
+        listbox.insert(tk.END, f"Task {i}: {task[0]} | Deadline: {' '.join(map(str, task[1]))} | Status: {task[2]}")
+
+def complete_task_gui():
+    selected_task_index = listbox.curselection()
+
+    if not selected_task_index:
+        messagebox.showwarning("선택 오류", "완료할 Task를 선택하세요.")
+        return
+
+    task_index = int(selected_task_index[0])
+    if not tasks[task_index][2]:
+        del tasks[task_index]
+        update_listbox()
+    else:
+        messagebox.showinfo("이미 완료됨", "이미 완료된 Task입니다.")
+
+# # Terminal 시작
+load_tasks()
+# while True:
+#     select = input("1. Task를 추가\n2. Order순으로 Task를 정렬\n3. Deadline순으로 Task를 정렬\n4. Task를 완료\n5. Task Manager를 종료\n")
+#     if select == '1':
+#         add_task()
+#     elif select == '2':
+#         sort_by_order()
+#     elif select == '3':
+#         sort_by_deadline()
+#     elif select == '4':
+#         complete_task()
+#     elif select == '5':
+#         break
+#     else:
+#         print("유효하지 않은 input. 알맞은 option을 선택.")    
+
+
+# GUI 시작
+window=tk.Tk()
+
+# 창 이름 설정
+window.title("Task Manager")
+# 초기 창 크기
+window.geometry("480x640")
+# 창 크기 조절 여부
+window.resizable(False, True)
+
+# 이 아래는 chat-GPT draft임.
+
+# Task 입력을 위한 Entry
+label_task = tk.Label(window, text="Task 내용:")
+label_task.pack(pady=5)
+entry_task = tk.Entry(window, width=30)
+entry_task.pack(pady=5)
+
+# Deadline 입력을 위한 Entry
+label_deadline = tk.Label(window, text="Deadline (년 월 일):")
+label_deadline.pack(pady=5)
+entry_deadline = tk.Entry(window, width=30)
+entry_deadline.pack(pady=5)
+
+# 추가 버튼
+button_add = tk.Button(window, text="Task 추가", command=add_task_gui)
+button_add.pack(pady=10)
+
+# Task 리스트 표시를 위한 Listbox
+listbox = tk.Listbox(window, width=50, height=15)
+listbox.pack(pady=10)
+
+button_sort_order = tk.Button(window, text="Order순으로 정렬", command=sort_by_order_gui)
+button_sort_order.pack(pady=5)
+
+button_sort_deadline = tk.Button(window, text="Deadline순으로 정렬", command=sort_by_deadline_gui)
+button_sort_deadline.pack(pady=5)
+
+button_complete_task = tk.Button(window, text="Task 완료로 표시", command=complete_task_gui)
+button_complete_task.pack(pady=5)
+
+
+window.mainloop()
+
+# 끝나고 나서
+save_tasks()
